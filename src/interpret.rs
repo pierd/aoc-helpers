bd901e452f411fa497c0e9b0ebb1adae733d46d7
Kuba Jaroszewski <jakub.jaroszewski@gmail.com>
2022-06-07 14:25:26 -0500

    Use Execute trait instead of helper function

--- src/interpret.rs
+++ src/interpret.rs
@@ -11,31 +11,28 @@ impl Default for Jump {
     }
 }
 
-pub trait Instruction<S> {
+pub trait Execute<S> {
     fn execute(&self, state: S) -> (S, Jump);
 }
 
-pub fn execute_all<S, I: Instruction<S>>(initial: S, instrs: &[I]) -> S {
-    let mut state = initial;
-    let mut instr_idx = 0;
-    while let Some(instr) = instrs.get(instr_idx) {
-        let (new_state, jump) = instr.execute(state);
-        state = new_state;
-        match jump {
-            Jump::Absolute(idx) => instr_idx = idx,
-            Jump::Relative(d) => {
-                if let Ok(new_idx) = usize::try_from(instr_idx as isize + d) {
-                    instr_idx = new_idx
-                } else {
-                    break;
+impl<S, E: Execute<S>> Execute<S> for &[E] {
+    fn execute(&self, mut state: S) -> (S, Jump) {
+        let mut instr_idx = 0;
+        while let Some(instr) = self.get(instr_idx) {
+            let (new_state, jump) = instr.execute(state);
+            state = new_state;
+            match jump {
+                Jump::Absolute(idx) => instr_idx = idx,
+                Jump::Relative(d) => {
+                    if let Ok(new_idx) = usize::try_from(instr_idx as isize + d) {
+                        instr_idx = new_idx
+                    } else {
+                        break;
+                    }
                 }
+                Jump::Stop => break,
             }
-            Jump::Stop => break,
         }
+        (state, Jump::Stop)
     }
-    state
-}
-
-pub fn execute_all_default<S: Default, I: Instruction<S>>(instrs: &[I]) -> S {
-    execute_all(Default::default(), instrs)
 }
